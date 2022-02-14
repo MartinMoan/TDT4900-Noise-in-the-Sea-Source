3be8c5648ce991d2f6520d4f2a8271a7a8edd03f
MartinMoan <martin.moan1@gmail.com>
2022-02-15 00:37:16 +0100

    [patch] CUDA bug

--- tracking/sheets.py
+++ tracking/sheets.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
+import pathlib
+
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+# define the scope
+# SCOPES = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+SCOPES = ['https://www.googleapis.com/auth/drive']
+SPREADSHEET_ID = '1qT3gS0brhu2wj59cyeZYP3AywGErROJCqR2wYks6Hcw'
+
+def authenticate():
+    credentials_path = pathlib.Path(__file__).parent.joinpath("credentials.json")
+    creds = ServiceAccountCredentials.from_json_keyfile_name(credentials_path, SCOPES)
+    client = gspread.authorize(creds)
+    return client
 
 def main():
-    pass
+    client = authenticate()
+    sheet = client.open_by_key(SPREADSHEET_ID)
+
+    for worksheet in sheet.worksheets():
+        df = pd.DataFrame.from_dict(worksheet.get_all_records())
+        print(df)
 
 if __name__ == "__main__":
     main()
