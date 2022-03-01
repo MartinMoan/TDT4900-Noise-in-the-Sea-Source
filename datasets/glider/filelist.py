7c060603e2ebafaef049d908dee33f999f342edf
MartinMoan <martin.moan1@gmail.com>
2022-03-01 20:43:29 +0100

    [minor] bugfix

--- datasets/glider/filelist.py
+++ datasets/glider/filelist.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python3
+import multiprocessing
 import pathlib
 import subprocess
+from multiprocessing.pool import Pool, ThreadPool
 import re
 import sys
 from datetime import datetime, timedelta
 from utils import get_wav_info
 import pandas as pd
+import numpy as np
 import git
 from rich import print
 
 sys.path.insert(0, str(pathlib.Path(git.Repo(pathlib.Path(__file__).parent, search_parent_directories=True).working_dir)))
 import config
 
-def get_filelist(dev=False, reload_files=True, printskip=100):
+def _info(args):
+    index, max, filepath = args
+    if index % 100 == 0:
+        print(index, max, filepath)
+    return get_wav_info(filepath)
+        
+def get_file_info(local_filelist):
+    with Pool(processes=int(multiprocessing.cpu_count() - 1)) as pool:
+        ids = range(len(local_filelist))
+        maxid = np.full(len(local_filelist), len(local_filelist))
+        items = zip(ids, maxid, local_filelist)
+        return pool.map(_info, items)
+
+def get_filelist(dev=False, reload_files=True):
     if reload_files:
         data = {}
         local_filelist = config.list_local_audiofiles()
-        for index, file in enumerate(local_filelist):
-            if index % printskip == 0:
-                print(index, len(local_filelist), file.name)
-            info = get_wav_info(file)
+        fileinfo = get_file_info(local_filelist)
+        for index, info in enumerate(fileinfo):            
             if info is not None:
                 for key in info.keys():
                     if key not in data.keys():
@@ -32,6 +46,7 @@ def get_filelist(dev=False, reload_files=True, printskip=100):
 
         df = pd.DataFrame(data=data)
         df.to_csv(config.AUDIO_FILE_CSV_PATH, index=False)
+        print(df)
         return df
     else:
         df = pd.read_csv(config.AUDIO_FILE_CSV_PATH)
