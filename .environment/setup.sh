b2500a26583202697634f16dff7119cd8eabc0b2
MartinMoan <martin.moan1@gmail.com>
2022-03-30 01:08:04 +0200

    [minor] environment issues

--- .environment/setup.sh
+++ .environment/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+conda init
 conda deactivate
 conda env remove --name TDT4900
-conda env create --file environment.yml
+conda env create --file ../environment.yml
 conda activate TDT4900
+pip install timm==0.4.5
 # Ensure that the directory /cluster/work/<username> exists. 
