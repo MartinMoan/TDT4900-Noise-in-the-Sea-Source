43f22034944f64a86cf9ba99bcd47117249bf541
MartinMoan <martin.moan1@gmail.com>
2022-04-30 17:00:01 +0200

    [minor] stuff

--- datasets/provider.py
+++ datasets/provider.py
@@ -7,7 +7,7 @@ from typing import Mapping, Union
 import git
 
 sys.path.insert(0, str(pathlib.Path(git.Repo(pathlib.Path(__file__).parent, search_parent_directories=True).working_dir)))
-from interfaces import IDatasetProvider, ITensorAudioDataset, ICustomDataset, IDatasetBalancer, IFeatureAccessor, ILabelAccessor
+from interfaces import IDatasetProvider, ITensorAudioDataset, ICustomDataset, IDatasetBalancer, IFeatureAccessor, ILabelAccessor, ILoggerFactory
 from datasets.limiting import DatasetLimiter
 from datasets.tensordataset import TensorAudioDataset
 
@@ -31,7 +31,8 @@ class VerificationDatasetProvider(IDatasetProvider):
         randomize: bool,
         balanced: bool,
         feature_accessor: IFeatureAccessor,
-        label_accessor: ILabelAccessor) -> None:
+        label_accessor: ILabelAccessor,
+        logger_factory: ILoggerFactory) -> None:
 
         self.limit = limit
         self.feature_accessor = feature_accessor
@@ -52,7 +53,8 @@ class VerificationDatasetProvider(IDatasetProvider):
         self.tensordataset = TensorAudioDataset(
             dataset=self.limited,
             label_accessor=label_accessor,
-            feature_accessor=feature_accessor
+            feature_accessor=feature_accessor,
+            logger_factory=logger_factory
         )
         
     def provide(self) -> ITensorAudioDataset:
