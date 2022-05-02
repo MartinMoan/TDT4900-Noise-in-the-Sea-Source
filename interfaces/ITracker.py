2c69e49b590383ae3c0ef5cc0e1e44c20d1f4040
MartinMoan <martin.moan1@gmail.com>
2022-05-02 11:53:59 +0200

    [minor] added track_dataset method to ITracker interface definition, to track limited number of example spectrograms

--- interfaces/ITracker.py
+++ interfaces/ITracker.py
@@ -4,7 +4,13 @@ import pathlib
 
 from typing import Mapping, Union
 
+from .ITensorAudioDataset import ITensorAudioDataset
+
 class ITracker(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def track(trackables: Mapping[str, any]) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def track_dataset(dataset: ITensorAudioDataset) -> None:
         raise NotImplementedError
