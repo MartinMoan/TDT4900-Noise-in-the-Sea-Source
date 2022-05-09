6df7915f7a277bcde51f41e7e4f5e8a5a110524a
MartinMoan <martin.moan1@gmail.com>
2022-05-09 21:56:22 +0200

    [patch] stuff

--- tracking/datasettracker.py
+++ tracking/datasettracker.py
@@ -12,61 +12,61 @@ import config
 from datasets.tensordataset import TensorAudioDataset
 
 def track_dataset(dataset: TensorAudioDataset, n_examples: int = 50):
-        if n_examples <= 0 or len(dataset) < n_examples:
-            return
-        
-        indeces = np.random.random_integers(0, len(dataset) - 1, n_examples)
+    if n_examples <= 0 or len(dataset) < n_examples:
+        return
+    
+    indeces = np.random.random_integers(0, len(dataset) - 1, n_examples)
 
-        table = wandb.Table(columns=[
-            "audio", 
-            "spectrogram", 
-            "source_class_specific", 
-            "clip_classes", 
-            "dataset_classes", 
-            "tensor_label", 
-            "filename", 
-            "file_start_time", 
-            "file_end_time", 
-            "clip_offset_seconds", 
-            "clip_duration_seconds",
-            "clip_index"
-        ])
+    table = wandb.Table(columns=[
+        "audio", 
+        "spectrogram", 
+        "source_class_specific", 
+        "clip_classes", 
+        "dataset_classes", 
+        "tensor_label", 
+        "filename", 
+        "file_start_time", 
+        "file_end_time", 
+        "clip_offset_seconds", 
+        "clip_duration_seconds",
+        "clip_index"
+    ])
 
-        for index in range(len(indeces)):
-            i = indeces[index]
-            clip = dataset.audiodata(i)
-            X, Y = dataset[i]
-            audio = wandb.Audio(clip.samples, sample_rate=clip.sampling_rate)
-            spectrogram = wandb.Image(X, caption=f"{clip.filepath.name}")
-            source_class_specific = ", ".join(np.sort(clip.labels.source_class_specific.unique(), axis=0))
-            clip_classes = ", ".join(np.sort(clip.labels.source_class.unique(), axis=0))
-            dataset_classes = str(dataset.classes())
-            tensor_label = str(Y.detach().numpy())
-            filename = clip.filepath.name
-            datetimeformat = "%c"
-            file_start_time = clip.file_start_time.strftime(datetimeformat)
-            file_end_time = clip.file_end_time.strftime(datetimeformat)
-            clip_offset = clip.clip_offset
-            clip_duration = clip.clip_duration
+    for index in range(len(indeces)):
+        i = indeces[index]
+        clip = dataset.audiodata(i)
+        X, Y = dataset[i]
+        audio = wandb.Audio(clip.samples, sample_rate=clip.sampling_rate)
+        spectrogram = wandb.Image(X, caption=f"{clip.filepath.name}")
+        source_class_specific = ", ".join(np.sort(clip.labels.source_class_specific.unique(), axis=0))
+        clip_classes = ", ".join(np.sort(clip.labels.source_class.unique(), axis=0))
+        dataset_classes = str(dataset.classes())
+        tensor_label = str(Y.detach().numpy())
+        filename = clip.filepath.name
+        datetimeformat = "%c"
+        file_start_time = clip.file_start_time.strftime(datetimeformat)
+        file_end_time = clip.file_end_time.strftime(datetimeformat)
+        clip_offset = clip.clip_offset
+        clip_duration = clip.clip_duration
 
-            table.add_data(
-                audio, 
-                spectrogram, 
-                source_class_specific, 
-                clip_classes, 
-                dataset_classes, 
-                tensor_label,
-                filename,
-                file_start_time,
-                file_end_time,
-                clip_offset,
-                clip_duration,
-                i
-            )
-            print(f"Logging dataset example {index} / {len(indeces)}")
+        table.add_data(
+            audio, 
+            spectrogram, 
+            source_class_specific, 
+            clip_classes, 
+            dataset_classes, 
+            tensor_label,
+            filename,
+            file_start_time,
+            file_end_time,
+            clip_offset,
+            clip_duration,
+            i
+        )
+        print(f"Logging dataset example {index} / {len(indeces)}")
 
-        wandb.log({"examples": table})
-        wandb.config.update(dict(
-            example_shape=dataset.example_shape(),
-            label_shape=dataset.label_shape()
-        ))
+    wandb.log({"examples": table})
+    wandb.config.update(dict(
+        example_shape=dataset.example_shape(),
+        label_shape=dataset.label_shape()
+    ))
