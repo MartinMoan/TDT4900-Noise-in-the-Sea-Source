09b8278d50383394f3c6dea2421c8089d84e89b0
MartinMoan <martin.moan1@gmail.com>
2022-05-15 13:25:51 +0200

    [patch] checking slurm variables

---
+++ experiments/DistributedTrainingTest/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import os
+from pprint import pprint
+
+slurm_variables = {key: value for key, value in os.environ if "SLURM" in key}
+check_these = ["SLURM_NODEID", "SLURM_TASK_PID", "SLURM_PROCID", "SLURM_JOB_GID", "SLURM_JOBID", "SLURM_LOCALID"]
+to_check = {key: os.environ.get(key) for key in check_these}
+
+print(to_check)
+print()
+print(slurm_variables)
+print()
+print()
+print()
+
+
