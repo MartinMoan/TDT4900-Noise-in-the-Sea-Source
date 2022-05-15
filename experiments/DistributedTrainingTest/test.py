4b75f61d92a1206f8080ba3b7ca963bfafe704d4
MartinMoan <martin.moan1@gmail.com>
2022-05-15 13:27:57 +0200

    [patch] stuff

--- experiments/DistributedTrainingTest/test.py
+++ experiments/DistributedTrainingTest/test.py
@@ -2,7 +2,7 @@
 import os
 from pprint import pprint
 
-slurm_variables = {key: value for key, value in os.environ if "SLURM" in key}
+slurm_variables = {key: value for key, value in os.environ.items() if "SLURM" in key}
 check_these = ["SLURM_NODEID", "SLURM_TASK_PID", "SLURM_PROCID", "SLURM_JOB_GID", "SLURM_JOBID", "SLURM_LOCALID"]
 to_check = {key: os.environ.get(key) for key in check_these}
 
