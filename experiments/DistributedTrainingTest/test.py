31e35170a0b45ef9b08f295ce20420d47f1cfaa7
MartinMoan <martin.moan1@gmail.com>
2022-05-16 19:46:30 +0200

    [patch] srun check

--- experiments/DistributedTrainingTest/test.py
+++ experiments/DistributedTrainingTest/test.py
@@ -1,11 +1,6 @@
-#!/usr/bin/env python3
 import os
-from pprint import pprint
+import torch
 
-slurm_variables = {key: value for key, value in os.environ.items() if "SLURM" in key}
-check_these = ["SLURM_NODEID", "SLURM_TASK_PID", "SLURM_PROCID", "SLURM_JOB_GID", "SLURM_JOBID", "SLURM_LOCALID"]
-to_check = {key: os.environ.get(key) for key in check_these}
-
-print(os.environ.get("SLURM_PROCID"))
-print()
-
+procid = os.environ.get("SLURM_PROCID")
+print(f"SLURM_PROCID: {procid}")
+print(f"Number of gpus available: {torch.cuda.device_count()}")
