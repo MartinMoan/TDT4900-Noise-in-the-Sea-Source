c59bdb9875ad509af3f79715b61498b50e2fbd3f
MartinMoan <martin.moan1@gmail.com>
2022-05-23 16:47:54 +0200

    [minor] added SSAST dataloader

---
+++ experiments/SelfSupervisedAST/train.py
@@ -0,0 +1,11 @@
+
+
+def main(hparams: argparse.Namespace) -> None:
+    pass
+
+def init() -> argparse.Namespace:
+    return None
+
+if __name__ == "__main__":
+    hparams = init()
+    main(hparams)
